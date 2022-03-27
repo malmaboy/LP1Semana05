a3b6449de819a22af19d06c299cf4842a1ea6b95
Hugo Carvalho <huguinho66@live.com>
2022-03-28 00:34:09 +0100

    exercices 7 and 8 added

---
+++ Chronos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program;
+            program = new Program();
+            program.Run();
+        }
+
+
+        private void Run()
+        {
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+            cron1.Start();
+            Thread.Sleep(1000);
+            cron2.Start();
+            Thread.Sleep(1000);
+            cron1.Stop();
+            cron2.Stop();
+
+            TimeSpan time1 = cron1.Elapsed;
+            TimeSpan time2 = cron2.Elapsed;
+            
+            
+            Console.WriteLine($" The Values are in seconds \n" +
+                              $" cron1 = {time1.Seconds}s" +
+                              $" \n cron2 = {time2.Seconds}s");
+        }
+    }
+}
---
+++ MyTestOfRandoms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        private Random random;
+
+        private Program()
+        {
+            random = new Random();
+        }
+        
+        static void Main(string[] args)
+        {
+            Program program;
+            program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            int n = 0;
+            int sum = 0;
+            int dice = 0;
+            Console.WriteLine("Insert the numbers of dices");
+            int dices = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < dices; i++)
+            {
+                n++;
+                Console.WriteLine( $"dice nº{n} = { dice = random.Next(1,7)}");
+                
+                Console.WriteLine($"sum = {sum = sum + dice }");
+            }
+        }
+    }
+}
